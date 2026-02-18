ba3630487a33744c84ab4f96f1ef664836955c52
Lyyn <vladlyyn@gmail.com>
2026-02-18 11:54:09 +0300

    Add test for button add_to_basket

--- test_items.py
+++ test_items.py
@@ -1,1 +1,15 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
+
+def test_button_add_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(10)
+    basket_button = WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.btn-add-to-basket')))
+    assert basket_button.is_displayed()
+
+    print(f"Текст кнопки: {basket_button.text}")
+    
